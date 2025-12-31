a75700fff5df3c4e0fd56581e5b705243e3f65a1
Muhammad Shekh Yaseen <casemng@gmail.com>
2026-01-01 01:45:55 +0300

    edite Firebase implementation

--- LaylaApi/Services/FirebaseServices/ServiceCollectionExtensions/FirebaseServicesCollectionExtensions.cs
+++ LaylaApi/Services/FirebaseServices/ServiceCollectionExtensions/FirebaseServicesCollectionExtensions.cs
@@ -16,8 +16,10 @@ namespace LaylaApi.Services.FirebaseServices.ServiceCollectionExtensions
         public static void AddFirebaseApp(this WebApplicationBuilder builder)
         {
             var path = builder.Configuration["FIREBASE_CREDENTIALS_PATH"];
+
             if (!File.Exists(path))
                 throw new Exception("Firebase credentials file not found");
+
             if (string.IsNullOrWhiteSpace(path))
                 throw new Exception("FIREBASE_CREDENTIALS_PATH Problem");
 
