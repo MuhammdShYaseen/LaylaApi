15ba9794f003ab0134962e02015681c8f98e3dfc
Muhammad Shekh Yaseen <casemng@gmail.com>
2026-01-05 07:21:46 +0300

    add chat hub

---
+++ LaylaApi/SignalR_Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace LaylaApi.SignalR_Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinConversation(int conversationId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId,
+            conversationId.ToString());
+        }
+
+    }
+}
