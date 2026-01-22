37bda1c121a302deb76391ed9705900d9951b0e0
Muhammad Shekh Yaseen <casemng@gmail.com>
2026-01-23 00:56:51 +0300

    clean entities from common fields

--- LaylaApi/Models/MainModels/Conversation.cs
+++ LaylaApi/Models/MainModels/Conversation.cs
@@ -3,15 +3,13 @@ using LaylaApi.DomainEvents.Domain.Common;
 
 namespace LaylaApi.Models.MainModels
 {
-    public class Conversation 
+    public class Conversation : Entity
     {
-        public int Id { get; set; }  
         public int ApartmentId { get; set; }
         public Apartment? Apartment { get; set; }
         public int OwnerId { get; set; }
         public int UserId { get; set; }
         public bool IsClosedByOwner { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public ICollection<Message>? Messages { get; set; }
 
         public static Conversation Create(int apartmentId, int ownerId, int userId)
@@ -22,6 +20,7 @@ namespace LaylaApi.Models.MainModels
                 OwnerId = ownerId,
                 UserId = userId,
                 CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
                 IsClosedByOwner = false
             };
         }
